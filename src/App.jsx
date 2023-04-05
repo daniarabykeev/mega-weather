af0501c34310d84bd37080f0f3ed2bdd6104c500
daniarabykeev <daniarabykeev@gmail.com>
2023-04-05 23:37:01 +0600

    init: first commit

---
+++ src/App.jsx
@@ -0,0 +1,169 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import * as icons from "./assets/icons";
+import theme from "./theme";
+
+function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [isDark, setIsDark] = useState(true);
+  const [isCelcium, setIsCelcium] = useState(true);
+
+  useEffect(() => {
+    if (isDark) {
+      for (let i in theme.dark) {
+        document.documentElement.style.setProperty(i, theme.dark[i]);
+      }
+    } else {
+      for (let i in theme.light) {
+        document.documentElement.style.setProperty(i, theme.light[i]);
+      }
+    }
+  }, [isDark]);
+
+  function getData(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4f76ef8a1a352c2cf0769656549175da`;
+    axios.get(url).then((response) => {
+      setData(response.data);
+      console.log(response.data);
+    });
+  }
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      getData(location);
+      setLocation("");
+    }
+  };
+
+  useEffect(() => {
+    axios.get("https://ipapi.co/json").then(({ data }) => {
+      getData(data.city);
+    });
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          placeholder="Enter location"
+          value={location}
+          type="text"
+          autoFocus
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyDown={searchLocation}
+        />
+        {/* <input
+          type="checkbox"
+          checked={isDark}
+          onChange={(e) => setIsDark(e.target.checked)}
+        /> */}
+      </div>
+
+      <label
+        role="button"
+        for="checkbox"
+        class="switch"
+        checked={isCelcium}
+        onChange={(e) => setIsCelcium(e.target.checked)}
+      >
+        <input type="checkbox" id="checkbox" />
+        <span class="switch__ball"></span>
+        <i class="ri-sun-line switch__sun"></i>
+        <i class="ri-moon-line switch__moon"></i>
+      </label>
+
+      <label
+        role="button"
+        for="checkbox1"
+        class="switch"
+        checked={isDark}
+        onChange={(e) => setIsDark(e.target.checked)}
+      >
+        <input type="checkbox" id="checkbox1" />
+        <span class="switch__ball"></span>
+        <i class="ri-sun-line switch__sun"></i>
+        <i class="ri-moon-line switch__moon"></i>
+      </label>
+
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? (
+              isCelcium ? (
+                <h1>{Math.round(data.main.temp - 273.15)}°C</h1>
+              ) : (
+                <h1>
+                  {Math.round((data.main.temp - 273.15) * (9 / 5) + 32)}°F
+                </h1>
+              )
+            ) : null}
+            <div className="icon">
+              {data.main ? (
+                <img src={icons[`icon_${data.weather[0].icon}`]} alt="icon" />
+              ) : null}
+            </div>
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name != undefined && (
+          <div className="bottom">
+            <div className="bottom-temp">
+              {data.main ? (
+                isCelcium ? (
+                  <p className="bold">
+                    {Math.round(data.main.feels_like - 273.15)}°C
+                  </p>
+                ) : (
+                  <p className="bold">
+                    {Math.round((data.main.feels_like - 273.15) * (9 / 5) + 32)}
+                    °F
+                  </p>
+                )
+              ) : null}
+              <p>Feels like</p>
+            </div>
+            <div className="bottom-temp">
+              {data.main ? (
+                isCelcium ? (
+                  <p className="bold">
+                    {Math.round(data.main.temp_min - 273.15)}°C
+                  </p>
+                ) : (
+                  <p className="bold">
+                    {Math.round((data.main.temp_min - 273.15) * (9 / 5) + 32)}
+                    °F
+                  </p>
+                )
+              ) : null}
+              <p>min</p>
+            </div>
+            <div className="bottom-temp">
+              {data.main ? (
+                isCelcium ? (
+                  <p className="bold">
+                    {Math.round(data.main.temp_max - 273.15)}°C
+                  </p>
+                ) : (
+                  <p className="bold">
+                    {Math.round((data.main.temp_max - 273.15) * (9 / 5) + 32)}
+                    °F
+                  </p>
+                )
+              ) : null}
+              <p>max </p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export default {
+  dark: {
+    "--background": "rgba(0, 0, 0, 0.4)",
+    "--font-color": "rgb(102, 183, 60)",
+    "--input-bgc": "rgb(38, 22, 91);",
+    "--input-color": "rgb(102, 183, 60)",
+  },
+  light: {
+    "--background": "rgba(255, 255, 255, 1)",
+    "--font-color": "#000000",
+    "--input-bgc": "#fff",
+    "--input-color": "#000000",
+  },
+};
